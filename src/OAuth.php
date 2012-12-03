baded167fdd780bdf7426cc8fddd622e7c8e3c3c
David Grudl <david@grudl.com>
2012-12-03 02:18:49 +0100

    fixed OAuthUtil not found [Closes #13]

--- src/OAuth.php
+++ src/OAuth.php
@@ -762,7 +762,7 @@ class Twitter_OAuthUtil
 {
 	public static function urlencode_rfc3986($input) {
 		if (is_array($input)) {
-			return array_map(array('OAuthUtil', 'urlencode_rfc3986'), $input);
+			return array_map(array('Twitter_OAuthUtil', 'urlencode_rfc3986'), $input);
 		} else if (is_scalar($input)) {
 			return str_replace(
 				'+',
