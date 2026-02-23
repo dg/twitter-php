3d566a23014033bb2bf033feb6b2a55a9df226ba
David Grudl <david@grudl.com>
2026-02-23 01:11:51 +0100

    singleline declare statements

--- src/Twitter.php
+++ src/Twitter.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 /**
  * Twitter for PHP - library for sending messages to Twitter and receiving status updates.
--- src/twitter.class.php
+++ src/twitter.class.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 require_once __DIR__ . '/OAuth.php';
 require_once __DIR__ . '/Twitter.php';
